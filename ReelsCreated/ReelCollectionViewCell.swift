15649125aa60bbe94329a9851bc0ec50639fe807
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-25 12:35:17 -0700

    ommit

--- ReelsCreated/ReelCollectionViewCell.swift
+++ ReelsCreated/ReelCollectionViewCell.swift
@@ -4,9 +4,142 @@
 //
 //  Created by Arpit iOS Dev. on 25/05/24.
 //
-
-import UIKit
+import AVKit
 
 class ReelCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "ReelCollectionViewCell"
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
+    
+    private let usernameLable: UILabel = {
+        let label  = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let captionLable: UILabel = {
+        let label  = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    private let audioLable: UILabel = {
+        let label  = UILabel()
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    // Button
+    
+    private let profileButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "04"), for: .normal)
+       
+       return button
+    }()
+    
+    private let likeButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "heart"), for: .normal)
+        button.tintColor = .white
+       return button
+    }()
+    
+    private let commentButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "message"), for: .normal)
+        button.tintColor = .white
+       return button
+    }()
+    
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "paperplane"), for: .normal)
+        button.tintColor = .white
+       return button
+    }()
+    
+    private let moreButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "ellipsis"), for: .normal)
+        button.tintColor = .white
+       return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupPlayerLayer()
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupPlayerLayer() {
+        playerLayer = AVPlayerLayer()
+        playerLayer?.videoGravity = .resizeAspectFill
+        layer.addSublayer(playerLayer!)
+    }
+    
+    private func setupUI() {
+        // Add UI elements to the content view
+        contentView.addSubview(usernameLable)
+        contentView.addSubview(captionLable)
+        contentView.addSubview(audioLable)
+            
+        contentView.addSubview(profileButton)
+        contentView.addSubview(likeButton)
+        contentView.addSubview(commentButton)
+        contentView.addSubview(shareButton)
+        contentView.addSubview(moreButton)
+        
+        // Layout the UI elements
+        let size = contentView.frame.size.width / 7
+        let width = contentView.frame.size.width
+        let height = contentView.frame.size.height - 100
+        
+        moreButton.frame = CGRect(x: width-size, y: height-size+20, width: 40, height: 10)
+        shareButton.frame = CGRect(x: width-size, y: height-(size*2), width: 40, height: 40)
+        commentButton.frame = CGRect(x: width-size, y: height-(size*3)-10, width: 40, height: 40)
+        likeButton.frame = CGRect(x: width-size, y: height-(size*4)-30, width: 40, height: 40)
+        
+        
+        profileButton.frame = CGRect(x: 7, y: height-100, width: 40, height: 40)
+        
+        
+        audioLable.frame = CGRect(x: 10, y: height, width: width-size-10, height: 50)
+        captionLable.frame = CGRect(x: 10, y: height-60, width: width-size-10, height: 50)
+        usernameLable.frame = CGRect(x: 55, y: height-105, width: width-size-10, height: 50)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        playerLayer?.frame = bounds
+    }
+    
+    func configure(with reel: Reel) {
+        if let url = Bundle.main.url(forResource: reel.filename, withExtension: "mp4") {
+            player = AVPlayer(url: url)
+            playerLayer?.player = player
+            player?.play()
+        }
+        
+        usernameLable.text = reel.username
+        captionLable.text = reel.description
+        audioLable.text = reel.audioTrackname
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        player?.pause()
+        player = nil
+    }
 }
--- ReelsCreated/ReelsViewController.swift
+++ ReelsCreated/ReelsViewController.swift
@@ -4,14 +4,72 @@
 //
 //  Created by Arpit iOS Dev. on 25/05/24.
 //
+struct Reel {
+    let filename: String
+    let username: String
+    let description: String
+    let audioTrackname: String
+}
 
 import UIKit
 
-class ReelsViewController: UIViewController {
+class ReelsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    private var collectionView: UICollectionView!
+    private var reels = [Reel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
+        loadReels()
     }
- 
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 0
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        collectionView.register(ReelCollectionViewCell.self, forCellWithReuseIdentifier: ReelCollectionViewCell.identifier)
+        
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+    }
+    
+    private func loadReels() {
+        reels = [
+            Reel(filename: "video", username: "@arpitdhameliya", description: "This is the first video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video1", username: "@hirenkanani", description: "This is the second video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video2", username: "@harshil", description: "This is the Third video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video3", username: "@harshad", description: "This is the Four video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video4", username: "@vrushbhbhai", description: "This is the Five video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video5", username: "@darshandhameliya", description: "This is the Six video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video6", username: "@neelsutariya", description: "This is the Seven video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video7", username: "@veddhameliya", description: "This is the Eight video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video8", username: "@divyeshdhameliya", description: "This is the nine video description", audioTrackname: "Original Video song"),
+            Reel(filename: "video3", username: "@Harshkheni", description: "This is the Ten video description", audioTrackname: "Original Video song"),
+        ]
+        collectionView.reloadData()
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return reels.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReelCollectionViewCell.identifier, for: indexPath) as! ReelCollectionViewCell
+        cell.configure(with: reels[indexPath.row])
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width, height: view.frame.size.height)
+    }
 }
